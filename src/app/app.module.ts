665f2f4f4568fbe4e62c0645bb54cd3ec15fca0e
RICHARD <steyner.urupeque.s@gmail.com>
2023-07-15 19:09:29 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DockerNginxComponent } from './components/docker-nginx/docker-nginx.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DockerNginxComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/docker-nginx/docker-nginx.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-docker-nginx',
+  templateUrl: './docker-nginx.component.html',
+  styleUrls: ['./docker-nginx.component.css']
+})
+export class DockerNginxComponent {
+
+}
